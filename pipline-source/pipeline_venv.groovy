872bf7b112019f2a4036fbe4660b759dc44d361a
leonardo <angel.quingaluisa@gmail.com>
2019-06-04 15:46:50 -0500

    pipline env

---
+++ pipline-source/pipeline_venv.groovy
@@ -0,0 +1,40 @@
+node {
+    stage 'Checkout and Build'
+
+    createVirtualEnv 'env'
+    executeIn 'env', 'pip install -r requirements.txt'
+    executeIn 'env', './manage.py test'
+    executeIn 'env', './manage.py integration-test'
+
+    virtualEnv('true')
+    runCmd('pip install -r requirements.txt')
+
+}
+
+// one of the workaround
+def createVirtualEnv(String name) {
+    sh "virtualenv ${name}"
+}
+
+def executeIn(String environment, String script) {
+    sh "source ${environment}/bin/activate && " + script
+}
+
+// alternative workaround
+env.VENV_PATH = "${JENKINS_HOME}/.virtualenv/${JOB_NAME}/venv"
+
+def virtualEnv(String rebuild){
+    withEnv(["PATH+VEX=~/.local/bin"]){
+        if(rebuild == "true") {
+            sh "rm -rf ${env.VENV_PATH}"
+            sh "echo 'rebuild is true'"
+        }
+        sh returnStatus: true, script: "virtualenv ${env.VENV_PATH}"
+    }
+}
+
+def runCmd(String pyCmd){
+    withEnv(["PATH+VEX=~/.local/bin"]){
+        sh returnStatus: true, script: "vex --path=${env.VENV_PATH} ${pyCmd}"
+    }
+}

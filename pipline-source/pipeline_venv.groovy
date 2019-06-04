b69202d88d2033b36368b3ddb31d82c914262319
leonardo <angel.quingaluisa@gmail.com>
2019-06-04 16:12:02 -0500

    actualizar file

--- pipline-source/pipeline_venv.groovy
+++ pipline-source/pipeline_venv.groovy
@@ -1,21 +1,24 @@
+//author aquingaluisa copia del file  pipeline_venv_workarounds.groovy en github
+
 node {
     stage 'Checkout and Build'
 
     createVirtualEnv 'env'
     executeIn 'env', 'pip install -r requirements.txt'
     executeIn 'env', './manage.py test'
-    executeIn 'env', './manage.py integration-test'
+    //executeIn 'env', './manage.py integration-test'
 
     virtualEnv('true')
     runCmd('pip install -r requirements.txt')
 
 }
 
-// one of the workaround
+// crear del ambiente virtual  con python 3
 def createVirtualEnv(String name) {
     sh "virtualenv -p python3 ${name}"
 }
 
+//encargado de ejecutar los escripts en el env
 def executeIn(String environment, String script) {
     sh ". ${environment}/bin/activate && " + script
 }

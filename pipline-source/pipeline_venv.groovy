bb4fe8b595892aab04ca9f046b0ed1f758aee6de
leonardo <angel.quingaluisa@gmail.com>
2019-06-04 21:50:49 -0500

    deploy pipline

--- pipline-source/pipeline_venv.groovy
+++ pipline-source/pipeline_venv.groovy
@@ -12,6 +12,10 @@ node {
     //ejecutar test
     executeIn 'env', ' python manage.py test devops_env'
 
+    stage 'Deploy'
+
+    sh "deploy.sh"
+
     stage 'Publish results'
     slackSend color: "good", message: "Build successful: `${env.JOB_NAME}#${env.BUILD_NUMBER}` <${env.BUILD_URL}|Open in Jenkins>"
 
